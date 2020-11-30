3a353aa2dd156876aec951436cfbbf2c4b4c8fae
pablonataniel76 <pablo.nataniel76@gmail.com>
2020-11-29 21:43:42 -0400

    using api on user's list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { UserComponent } from './components/user/user.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
-
+import { ROUTES } from './app.routes'
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +19,9 @@ import { AboutComponent } from './components/about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot( ROUTES, { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  users: any = [];
+
+  constructor( private http: HttpClient) { 
+    console.log('Const home done');
+    this.http.get('https://api.github.com/users')
+    .subscribe( (data:any) => {
+      this.users = data
+      console.log(data)
+    });
+  }
 
   ngOnInit(): void {
   }
