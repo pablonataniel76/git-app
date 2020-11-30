51edeabef8b12e8443785b09dc92694c2abe41c1
pablonataniel76 <pablo.nataniel76@gmail.com>
2020-11-29 21:05:59 -0400

    Components and routes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserComponent } from './components/user/user.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    NavbarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
